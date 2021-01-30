1f239f06f220f2c1182920ffcd24512e234ef36c
WhyK <windchimeyk@gmail.com>
2021-01-30 22:56:15 +0900

    refactor: add return type

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@
  * @param arr Objectの配列
  * @param key 配列として取得したいkey
  */
-export const extractObjectValue = <T>(arr: T[], key: keyof T) =>
+export const extractObjectValue = <T>(arr: T[], key: keyof T): T[keyof T][] =>
   arr.map((item) => item[key]);
 
 /**
@@ -15,8 +15,8 @@ export const extractObjectValue = <T>(arr: T[], key: keyof T) =>
 export const narrowdownArrayObject = <T>(
   array: T[],
   target: keyof T,
-  keyword: T[keyof T],
-) => array.filter((object) => object[target] === keyword);
+  keyword: T[keyof T]
+): T[] => array.filter((object) => object[target] === keyword);
 
 /**
  * カクヨムの独自タグ（|《》や《《》》）を対応するタグに変換する
@@ -25,12 +25,17 @@ export const narrowdownArrayObject = <T>(
  */
 export const kakuyomuOriginalTagConvert = (
   str: string,
-  prefix: string = "util",
-) => {
-  const converted = str.replace(
-    /[|｜](.+?)《(.+?)》/g,
-    `<ruby class="${prefix}-ruby">$1<rt>$2</rt></ruby>`,
-  ).replace(/《《(.+?)》》/g, `<strong class="${prefix}-emphasis">$1</strong>`);
+  prefix: string = "util"
+): string => {
+  const converted = str
+    .replace(
+      /[|｜](.+?)《(.+?)》/g,
+      `<ruby class="${prefix}-ruby">$1<rt>$2</rt></ruby>`
+    )
+    .replace(
+      /《《(.+?)》》/g,
+      `<strong class="${prefix}-emphasis">$1</strong>`
+    );
   return converted;
 };
 
@@ -38,7 +43,9 @@ export const kakuyomuOriginalTagConvert = (
  * 非同期にファイルの存在確認を行なう
  * @param file ファイルのパス
  */
-export const isExistFile = async (file: string) => {
+export const isExistFile = async (
+  file: string
+): Promise<boolean | undefined> => {
   try {
     await Deno.stat(file);
     return true;
@@ -51,7 +58,7 @@ export const isExistFile = async (file: string) => {
  * 同期的にファイルの存在確認を行なう
  * @param file ファイルのパス
  */
-export const isExistFileSync = (file: string) => {
+export const isExistFileSync = (file: string): boolean | undefined => {
   try {
     Deno.statSync(file);
     return true;
@@ -65,7 +72,10 @@ export const isExistFileSync = (file: string) => {
  * @param rawdata ファイルに書き込むデータ
  * @param file 書き込むファイルのパス
  */
-export const writeFile = async (rawdata: string, file: string) => {
+export const writeFile = async (
+  rawdata: string,
+  file: string
+): Promise<void> => {
   const encoder = new TextEncoder();
   const data = encoder.encode(rawdata);
   await Deno.writeFile(file, data);
@@ -76,7 +86,7 @@ export const writeFile = async (rawdata: string, file: string) => {
  * @param rawdata ファイルに書き込むデータ
  * @param file 書き込むファイルのパス
  */
-export const writeFileSync = (rawdata: string, file: string) => {
+export const writeFileSync = (rawdata: string, file: string): void => {
   const encoder = new TextEncoder();
   const data = encoder.encode(rawdata);
   Deno.writeFileSync(file, data);
@@ -86,7 +96,7 @@ export const writeFileSync = (rawdata: string, file: string) => {
  * 非同期にファイル読込を行なう
  * @param file 読み込むファイルのパス
  */
-export const readFile = async (file: string) => {
+export const readFile = async (file: string): Promise<string> => {
   const decoder = new TextDecoder("utf-8");
   const data = await Deno.readFile(file);
   return decoder.decode(data);
@@ -96,7 +106,7 @@ export const readFile = async (file: string) => {
  * 同期的にファイル読込を行なう
  * @param file 読み込むファイルのパス
  */
-export const readFileSync = (file: string) => {
+export const readFileSync = (file: string): string => {
   const decoder = new TextDecoder("utf-8");
   const data = Deno.readFileSync(file);
   return decoder.decode(data);
@@ -106,7 +116,8 @@ export const readFileSync = (file: string) => {
  * 任意の数字を文字に変換し、任意の桁数になるよう先頭に0を追加する
  * @param num 変換したい数字
  */
-export const zeroPadding = (num: number) => String(num).padStart(2, "0");
+export const zeroPadding = (num: number): string =>
+  String(num).padStart(2, "0");
 
 interface ReadDateOptions {
   date?: string;
@@ -124,8 +135,8 @@ interface ReadDateReturnType {
 }
 /**
  * 日付に関するデータをObjectで返す
- * 
- * 日付はデフォルトではnumber、ゼロ詰めするとstringになる  
+ *
+ * 日付はデフォルトではnumber、ゼロ詰めするとstringになる
  * 曜日は日〜土の値で返ってくる
  * @param options.date 任意の日付（ex. 2020/12/12）を指定し、なければ今日を使う
  * @param options.zeropadding ゼロ詰めするかどうか
@@ -154,7 +165,7 @@ interface TypedFetchOptions extends RequestInit {
 
 /**
  * 型定義できるFetch APIラッパー試製版
- * 
+ *
  * @param options.url 取得対象のURL
  * @param options.wanttype 取得したいデータ形式
  * @param options.method HTTPメソッド（今のところGETしか使えないと思う）
@@ -171,7 +182,9 @@ interface TypedFetchOptions extends RequestInit {
  * @param options.signal
  * @param options.window
  */
-export const typedFetch = async <T>(options: TypedFetchOptions): Promise<T> => {
+export const typedFetch = async <T>(
+  options: TypedFetchOptions
+): Promise<string | T | ArrayBuffer> => {
   const res = await fetch(options.url, {
     mode: options.mode,
     method: options.method,
@@ -188,11 +201,11 @@ export const typedFetch = async <T>(options: TypedFetchOptions): Promise<T> => {
     window: options.window,
   });
 
-  let data: T;
+  let data: T | ArrayBuffer | string;
   if (options.wanttype === "json") data = await res.json();
   else if (options.wanttype === "arrayBuffer") {
-    data = await res.arrayBuffer() as any;
-  } else data = await res.text() as any;
+    data = await res.arrayBuffer();
+  } else data = await res.text();
 
   return data;
 };
