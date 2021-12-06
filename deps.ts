cfe75e42a64a14b41b1e8813757935e93f807317
WhyK <windchimeyk@gmail.com>
2021-12-07 00:53:05 +0900

    fix: delete import_map.json

---
+++ deps.ts
@@ -0,0 +1,6 @@
+export {
+  extname,
+  join,
+  resolve,
+} from "https://deno.land/std@0.108.0/path/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts";
--- test/text.test.ts
+++ test/text.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "asserts";
+import { assertEquals } from "../deps.ts";
 import {
   Days,
   kakuyomuOriginalTagConvert,
